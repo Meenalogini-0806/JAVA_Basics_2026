7f84f21aa66198b65c2e54228600e0748f987e4f
meenalojini920-cell <meenalojini920@gmail.com>
2026-03-24 13:10:22 +0530

    file change

--- Mar/03.03/user_input2.java
+++ Mar/03.03/user_input2.java
@@ -2,12 +2,17 @@ import java.util.Scanner;
 class user_input2{
 	public static void main(String args[]){
 		Scanner sc = new Scanner(System.in);
+		
 		System.out.print("num1 :");
 		int num1=sc.nextInt();
+		
 		System.out.print("num2 :");
 		int num2=sc.nextInt();
 		
-		int sum=num1-num2;
+		System.out.print("num3:");
+		int num3=sc.nextInt();
+		
+		int sum=num1-num2+num3;
 		System.out.print("The number is :"+sum);
 	}
 }
---
+++ Mar/EvenOdd.java
@@ -0,0 +1,12 @@
+class EvenOdd{
+	public static void main(String args[]){
+		Scanner sc = new Scanner (System.in);
+		int number = sc.nextInt(System.in);
+		
+		if(number % 2 == 0){
+			System.out.print("Even");
+		}else{
+			System.out.print("Odd");
+		}
+	}
+}
